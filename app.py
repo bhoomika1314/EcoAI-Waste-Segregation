6223e1b781701214ac63e5eb77fcbb1ce33bf8e5
bhoomika1314 <bhoomikabhoomi997@gmail.com>
2026-07-18 16:57:41 +0530

    Add app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template
+import os
 
 app = Flask(__name__)
-CORS(app)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def home():
-    return "EcoAI Smart Waste Segregation API is running!"
+    return render_template("index.html")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
+
     if "image" not in request.files:
         return jsonify({
             "error": "No image uploaded"
@@ -17,32 +24,24 @@ def predict():
 
     image = request.files["image"]
 
-    # Temporary prediction for testing
-    # AI model will be connected here later
-    filename = image.filename.lower()
+    if image.filename == "":
+        return jsonify({
+            "error": "Please select an image"
+        }), 400
 
-    if "plastic" in filename or "bottle" in filename:
-        waste_type = "Plastic"
-        message = "Plastic waste → Put it in the Recyclable Waste Bin."
-    elif "paper" in filename:
-        waste_type = "Paper"
-        message = "Paper waste → Put it in the Recyclable Waste Bin."
-    elif "glass" in filename:
-        waste_type = "Glass"
-        message = "Glass waste → Put it in the Recyclable Waste Bin."
-    elif "metal" in filename or "can" in filename:
-        waste_type = "Metal"
-        message = "Metal waste → Put it in the Recyclable Waste Bin."
-    elif "organic" in filename or "food" in filename:
-        waste_type = "Organic"
-        message = "Organic waste → Put it in the Compost/Wet Waste Bin."
-    else:
-        waste_type = "Unknown"
-        message = "Unable to identify the waste."
+    image_path = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        image.filename
+    )
+
+    image.save(image_path)
+
+    # Temporary prediction for testing
+    prediction = "Plastic"
 
     return jsonify({
-        "waste_type": waste_type,
-        "message": message
+        "prediction": prediction,
+        "message": "Waste identified successfully!"
     })
 
 
