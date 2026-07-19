b94ead5d5ef1e24949932ab9ffac74c190bdc1fa
bhoomika1314 <bhoomikabhoomi997@gmail.com>
2026-07-19 18:30:51 +0530

    Add app.py

--- app.py
+++ app.py
@@ -1,49 +1,27 @@
-from flask import Flask, request, jsonify, render_template
-import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
+CORS(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    return "EcoAI Backend is Running!"
 
 @app.route("/predict", methods=["POST"])
 def predict():
-
     if "image" not in request.files:
-        return jsonify({
-            "error": "No image uploaded"
-        }), 400
+        return jsonify({"error": "No image uploaded"})
 
     image = request.files["image"]
 
-    if image.filename == "":
-        return jsonify({
-            "error": "Please select an image"
-        }), 400
-
-    image_path = os.path.join(
-        app.config["UPLOAD_FOLDER"],
-        image.filename
-    )
-
-    image.save(image_path)
-
-    # Temporary prediction for testing
+    # Temporary prediction
     prediction = "Plastic"
 
     return jsonify({
         "prediction": prediction,
-        "message": "Waste identified successfully!"
+        "message": "Recycle in Plastic Bin ♻️"
     })
-
 
 if __name__ == "__main__":
     app.run(debug=True)
